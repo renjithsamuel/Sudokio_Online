b023bd0316715e687d378a2b7babe34f3daa43f8
renjithsamuel <ranjithsamuelking@gmail.com>
2023-06-30 20:04:44 +0530

    prototype 6.2 fixed UI

--- sudokio/src/components/SudokuElem.jsx
+++ sudokio/src/components/SudokuElem.jsx
@@ -14,7 +14,7 @@ function SudokuElem({elem,onclick,style,currX,currY,rowind,colind,currVal}) {
             <div className="elem" 
                     style={{backgroundColor:elem.fixed ?( (currX !=-1 && currY!=-1) &&((currX == rowind) || (currY==colind) || (((rowind >= (currX - currX%3)) && (rowind< (currX - currX%3) + 3)) 
                       && ((colind >= (currY - currY%3)) && (colind< (currY - currY%3) + 3)))))?('var(--secondary-hover-color)')
-                     :( "var(--secondary-color)") 
+                     :( "var(--temp-secondary-color)") 
                      :(style=='var(--extra-hover-color)' || (currX==rowind && currY==colind))?'var(--extra-hover-color)'
                      : (hovered)? "var(--hover-color)" 
                      : ((currX !=-1 && currY!=-1) &&((currX == rowind  && currY!=colind) 
